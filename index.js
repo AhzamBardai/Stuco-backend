20623952314ee7223f7c9a1e79c2f906e9115a4b
Ahzam <ahzam.bardai@outlook.com>
2021-10-04 11:50:03 -0500

    removed body parser

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ dotenv.config()
 const app = express()
 // add cors origins !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 app.use(cors())
-app.use(bodyParser.json({ extended: true, limit: '50mb' }))
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 // routes
